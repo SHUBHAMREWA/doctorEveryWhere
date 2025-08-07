2300f02e23ba56b412f500895edb9b822633b152
shubham <shubhamrewamp17@gmail.com>
2025-08-07 12:09:45 +0530

    adding diployLink and favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,6 +14,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+        <head>
+        <link rel="icon" href="/favicon.png" type="image/png" />
+      </head>
       <body className="min-h-screen bg-white">
         <Header />
         <ClientLayout>
